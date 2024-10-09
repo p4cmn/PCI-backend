b5812ec41ff06838f907246aa61627e4ebe02e64
p4cmn <i.am.artem.derkach@gmail.com>
2024-10-10 01:10:43 +0300

    Added multi-threaded file processing

--- database/pcidatabase.cpp
+++ database/pcidatabase.cpp
@@ -1,50 +1,166 @@
+// #include <fstream>
+// #include <sstream>
+// #include <iostream>
+// #include "database/PciDatabase.h"
+
+// bool PciDatabase::loadFromFile(const string &filePath) {
+//   using namespace std;
+//   ifstream file(filePath);
+//   if (!file.is_open()) {
+//     cerr << "Unable to open pci.ids file." << endl;
+//     return false;
+//   }
+
+//   string line;
+//   ULONG currentVendorID = 0;
+
+//   while (getline(file, line)) {
+//     if (line.empty() || line[0] == '#') {
+//       continue;
+//     }
+//     if (line[0] != '\t') {
+//       istringstream ss(line);
+//       ss >> hex >> currentVendorID;
+//       string vendorName;
+//       getline(ss, vendorName);
+//       vendorName.erase(0, vendorName.find_first_not_of(" \t"));
+//       vendorMap[currentVendorID] = vendorName;
+//     } else if (line[1] != '\t') {
+//       istringstream ss(line.substr(1));
+//       ULONG deviceID;
+//       ss >> hex >> deviceID;
+//       string deviceName;
+//       getline(ss, deviceName);
+//       deviceName.erase(0, deviceName.find_first_not_of(" \t"));
+//       deviceMap[{currentVendorID, deviceID}] = deviceName;
+//     } else {
+//       istringstream ss(line.substr(2));
+//       ULONG subVendorID, subDeviceID;
+//       ss >> hex >> subVendorID >> subDeviceID;
+//       string subsystemName;
+//       getline(ss, subsystemName);
+//       subsystemName.erase(0, subsystemName.find_first_not_of(" \t"));
+//       subsystemMap[{currentVendorID, subVendorID, subDeviceID}] = subsystemName;
+//     }
+//   }
+//   return true;
+// }
+
+// std::string PciDatabase::getVendorName(ULONG vendorId) const {
+//   auto it = vendorMap.find(vendorId);
+//   return it != vendorMap.end() ? it->second : "N/A";
+// }
+
+// std::string PciDatabase::getDeviceName(ULONG vendorId, ULONG deviceId) const {
+//   auto it = deviceMap.find({vendorId, deviceId});
+//   return it != deviceMap.end() ? it->second : "N/A";
+// }
+
+// std::string PciDatabase::getSubsystemName(ULONG vendorId, ULONG subVendorId, ULONG subDeviceId) const {
+//   auto it = subsystemMap.find({vendorId, subVendorId, subDeviceId});
+//   return it != subsystemMap.end() ? it->second : "N/A";
+// }
+
+
+#include "database/PciDatabase.h"
 #include <fstream>
 #include <sstream>
 #include <iostream>
-#include "database/PciDatabase.h"
+#include <thread>
+#include <vector>
+#include <mutex>
 
-bool PciDatabase::loadFromFile(const string &filePath) {
-  using namespace std;
-  ifstream file(filePath);
-  if (!file.is_open()) {
-    cerr << "Unable to open pci.ids file." << endl;
-    return false;
-  }
+std::mutex mapMutex; // Мьютекс для защиты доступа к общим ресурсам
 
-  string line;
-  ULONG currentVendorID = 0;
+// Функция для парсинга части файла
+void parseFileChunk(const std::vector<std::string> &lines,
+		    ULONG &currentVendorID,
+		    map<ULONG, std::string> &localVendorMap,
+		    map<std::pair<ULONG, ULONG>, std::string> &localDeviceMap,
+		    map<std::tuple<ULONG, ULONG, ULONG>, std::string> &localSubsystemMap) {
 
-  while (getline(file, line)) {
+  for (const auto &line : lines) {
     if (line.empty() || line[0] == '#') {
       continue;
     }
     if (line[0] != '\t') {
-      istringstream ss(line);
-      ss >> hex >> currentVendorID;
-      string vendorName;
-      getline(ss, vendorName);
+      std::istringstream ss(line);
+      ss >> std::hex >> currentVendorID;
+      std::string vendorName;
+      std::getline(ss, vendorName);
       vendorName.erase(0, vendorName.find_first_not_of(" \t"));
-      vendorMap[currentVendorID] = vendorName;
+      localVendorMap[currentVendorID] = vendorName;
     } else if (line[1] != '\t') {
-      istringstream ss(line.substr(1));
+      std::istringstream ss(line.substr(1));
       ULONG deviceID;
-      ss >> hex >> deviceID;
-      string deviceName;
-      getline(ss, deviceName);
+      ss >> std::hex >> deviceID;
+      std::string deviceName;
+      std::getline(ss, deviceName);
       deviceName.erase(0, deviceName.find_first_not_of(" \t"));
-      deviceMap[{currentVendorID, deviceID}] = deviceName;
+      localDeviceMap[{currentVendorID, deviceID}] = deviceName;
     } else {
-      istringstream ss(line.substr(2));
+      std::istringstream ss(line.substr(2));
       ULONG subVendorID, subDeviceID;
-      ss >> hex >> subVendorID >> subDeviceID;
-      string subsystemName;
-      getline(ss, subsystemName);
+      ss >> std::hex >> subVendorID >> subDeviceID;
+      std::string subsystemName;
+      std::getline(ss, subsystemName);
       subsystemName.erase(0, subsystemName.find_first_not_of(" \t"));
-      subsystemMap[{currentVendorID, subVendorID, subDeviceID}] = subsystemName;
+      localSubsystemMap[{currentVendorID, subVendorID, subDeviceID}] = subsystemName;
     }
   }
+}
+
+bool PciDatabase::loadFromFile(const std::string &filePath) {
+  std::ifstream file(filePath);
+  if (!file.is_open()) {
+    std::cerr << "Unable to open pci.ids file." << std::endl;
+    return false;
+  }
+
+  // Чтение всех строк из файла
+  std::vector<std::string> lines;
+  std::string line;
+  while (std::getline(file, line)) {
+    if (line.length() > 10000) { // Проверка на слишком длинную строку
+      std::cerr << "Line too long, skipping: " << line.length() << " characters." << std::endl;
+      continue;
+    }
+    lines.push_back(line);
+  }
+  file.close();
+
+  if (lines.empty()) {
+    std::cout << "File is empty or could not be read." << std::endl;
+    return false;
+  }
+
+  std::cout << "File loaded successfully, total lines: " << lines.size() << std::endl;
+
+  // Обработка всего файла в одном потоке (пока для отладки)
+  map<ULONG, std::string> localVendorMap;
+  map<std::pair<ULONG, ULONG>, std::string> localDeviceMap;
+  map<std::tuple<ULONG, ULONG, ULONG>, std::string> localSubsystemMap;
+  ULONG currentVendorID = 0;
+
+  try {
+    parseFileChunk(lines, currentVendorID, localVendorMap, localDeviceMap, localSubsystemMap);
+  } catch (const std::exception &ex) {
+    std::cerr << "Exception during parsing: " << ex.what() << std::endl;
+    return false;
+  }
+
+  // Перенос локальных карт в общие карты
+  {
+    std::lock_guard<std::mutex> lock(mapMutex);
+    vendorMap.insert(localVendorMap.begin(), localVendorMap.end());
+    deviceMap.insert(localDeviceMap.begin(), localDeviceMap.end());
+    subsystemMap.insert(localSubsystemMap.begin(), localSubsystemMap.end());
+  }
+
+  std::cout << "PCI database loaded successfully." << std::endl;
   return true;
 }
+
 
 std::string PciDatabase::getVendorName(ULONG vendorId) const {
   auto it = vendorMap.find(vendorId);
@@ -60,3 +176,4 @@ std::string PciDatabase::getSubsystemName(ULONG vendorId, ULONG subVendorId, ULO
   auto it = subsystemMap.find({vendorId, subVendorId, subDeviceId});
   return it != subsystemMap.end() ? it->second : "N/A";
 }
+
--- database/pcidatabase.h
+++ database/pcidatabase.h
@@ -1,3 +1,32 @@
+// #ifndef PCIDATABASE_H
+// #define PCIDATABASE_H
+
+// #include <map>
+// #include <tuple>
+// #include <string>
+// #include <windows.h>
+
+// using std::map;
+// using std::pair;
+// using std::tuple;
+// using std::string;
+
+// class PciDatabase {
+
+// private:
+//   map<ULONG, string> vendorMap;
+//   map<pair<ULONG, ULONG>, string> deviceMap;
+//   map<tuple<ULONG, ULONG, ULONG>, string> subsystemMap;
+
+// public:
+//   bool loadFromFile(const string &filePath);
+//   string getVendorName(ULONG vendorId) const;
+//   string getDeviceName(ULONG vendorId, ULONG deviceId) const;
+//   string getSubsystemName(ULONG vendorId, ULONG subVendorId, ULONG subDeviceId) const;
+// };
+
+// #endif // PCIDATABASE_H
+
 #ifndef PCIDATABASE_H
 #define PCIDATABASE_H
 
@@ -12,17 +41,24 @@ using std::tuple;
 using std::string;
 
 class PciDatabase {
-
 private:
-  map<ULONG, string> vendorMap;
-  map<pair<ULONG, ULONG>, string> deviceMap;
-  map<tuple<ULONG, ULONG, ULONG>, string> subsystemMap;
+  map<ULONG, string> vendorMap; // Карта для хранения информации о вендорах
+  map<pair<ULONG, ULONG>, string> deviceMap; // Карта для хранения информации об устройствах
+  map<tuple<ULONG, ULONG, ULONG>, string> subsystemMap; // Карта для хранения информации о подсистемах
 
 public:
+  // Загрузка данных из файла
   bool loadFromFile(const string &filePath);
+
+  // Получение имени вендора по идентификатору
   string getVendorName(ULONG vendorId) const;
+
+  // Получение имени устройства по идентификаторам вендора и устройства
   string getDeviceName(ULONG vendorId, ULONG deviceId) const;
+
+  // Получение имени подсистемы по идентификаторам вендора, субвендора и субустройства
   string getSubsystemName(ULONG vendorId, ULONG subVendorId, ULONG subDeviceId) const;
 };
 
 #endif // PCIDATABASE_H
+
